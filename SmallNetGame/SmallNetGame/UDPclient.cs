a9db154ffae6c3daf26dfaf2e74cb1723a62ca3f
Daniel Madsen <d-madsen@live.dk>
2017-11-09 12:08:12 +0100

    Added UPDserver Added UDPclient Added TCPserver

---
+++ SmallNetGame/SmallNetGame/UDPclient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPclient
+{
+    static void Main(string[] args)
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        //IPAddress broadcast = IPAddress.Parse("192.168.0.255");
+        IPAddress broadcast = IPAddress.Parse("127.0.0.1");
+
+        //byte[] sendbuf = Encoding.ASCII.GetBytes(args[0]);
+        byte[] sendbuf = Encoding.ASCII.GetBytes("Hallo");
+
+        IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+        s.SendTo(sendbuf, ep);
+
+        Console.WriteLine("Message sent to the broadcast address");
+        Console.ReadKey();
+    }
+}
